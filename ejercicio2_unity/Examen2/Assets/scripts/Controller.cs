b384338928c35596bc15f498912bc0207dd496b6
esasian <esasian@users.noreply.github.com>
2018-12-12 20:40:54 +0100

    examen

---
+++ ejercicio2_unity/Examen2/Assets/scripts/Controller.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+
+    private float ballMove = 0.1f;
+    void Start () {
+		
+	}
+	
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.position = transform.position + new Vector3(ballMove, 0, 0);
+           // print("A");
+        }
+        if(Input.GetKeyDown(KeyCode.D))
+        {
+            transform.position = transform.position + new Vector3(-ballMove, 0, 0);
+            //print("D");
+        }
+
+        if (transform.position.y <= -3.014445f)
+        {
+            ballMove = 0f;
+          //  print("caida");
+        }
+         
+
+    }
+}
